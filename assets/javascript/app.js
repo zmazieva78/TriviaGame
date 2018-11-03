94d87936c657cfbc6c329f7d029f6bfc1b0dd879
Zarina Mazieva <zmazieva78@gmail.com>
2018-11-02 22:40:49 -0700

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,90 @@ var losses = 0;
 
 var questions = [
     {
-        question: "What is the name of city hamburger was named after?",
+        question: "Hamburgers get their name from what European city?",
         choices: ["London", "Hamburg", "Paris", "Amstredam"],
         correctChoice: 1
     },
     {
-        question: "The biggest city in the World?",
-        choices: ["Hong Kong", "Moscow", "New York", "Tokyo"],
+        question: "A poke bowl is a diced raw fish dish that originated in which U.S. state?",
+        choices: ["Seattle", "New York", "Hawaii", "Houston"],
+        correctChoice: 2
+    },
+    {
+        question: "When a drink is served “on the rocks”, it is served with what?",
+        choices: ["Vodka", "Straws", "Whipped Cream","Ice Cubes"],
         correctChoice: 3
+
+    },
+    {
+        question: "What ingredient is added to white sugar to make brown sugar?",
+        choices: ["Molasses", "Caramel", "Chocolate", "Food coloring"],
+        correctChoice: 0
+
+    },
+    {
+        question: "Sriracha is type of hot sauce named after a city located in what country?",
+        choices: ["Taiwan", "Thailand", "Japan", "Korea"],
+        correctChoice: 1
+
+    },
+    {
+        question: "Malbec, Sangiovese, and Syrah are all a type of what?",
+        choices: ["Jin", "Sangria", "Wine", "Whiskey"],
+        correctChoice: 2
+
+    },
+    {
+        question: "In which country did cheddar cheese originate?",
+        choices: ["USA", "Switzerland", "England", "Holland"],
+        correctChoice: 2
+
+    },
+    {
+        question: "Arby’s is a fast-food restaurant chain specializing in sandwiches made with what main ingredient?",
+        choices: ["Roast beef", "Grilled Chicken", "Fish", "Pork"],
+        correctChoice: 1 
+
+    },
+    {
+        question: "Sushi is a type of cuisine that originated in what country?",
+        choices: ["China", "Thailand", "Vietnam", "Japan"],
+        correctChoice: 3
+
+    },
+    {
+        question: "The original Starbucks was established in 1971 in what U.S. city?",
+        choices: ["Seattle", "Bellevue", "Burien", "Tukwila"],
+        correctChoice: 0
+
+    },
+    {
+        question: "The first McDonald’s restaurant opened in which U.S. state?",
+        choices: ["Nevada", "California", "Texas", "Florida"],
+        correctChoice: 1
+
+    },
+    {
+        question: "“Granny Smith” is a popular type of which fruit?",
+        choices: ["Pear", "Peach", "Apple","Orange"],
+        correctChoice: 2
+
+    },
+    {
+        question: "Dijon mustard originated in the city of Dijon, located in what country?",
+        choices: ["France", "Germany", "Italy","Greece"],
+        correctChoice: 0
+
+    },
+    {
+        question:"What was the original flavor of the filling in Twinkies?",
+        choices: ["Apples", "Blueberries", "Strawberry","Banana"],
+        correctChoice: 3
+    },
+    {
+        question: "Pho is a popular noodle soup from what country?",
+        choices: ["Thailand", "Vietnam", "China", "Malaysia"],
+        correctChoice: 1
     }
 ];
 
